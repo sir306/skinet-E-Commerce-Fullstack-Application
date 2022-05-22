936d5ebe3bacb97dc68f6d178bba9884521e38dc
sir306 <hardingnick00@gmail.com>
2022-05-22 21:12:18 +1200

    end of section 3
    added seed data and created apis to get retrieve this data

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -17,11 +17,11 @@ namespace API.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IProductRepository _repo;
-        
+
         public ProductController(IProductRepository repo)
         {
-           _repo = repo;
-          
+            _repo = repo;
+
         }
 
         [HttpGet]
@@ -37,6 +37,17 @@ namespace API.Controllers
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
         }
     }
 }
