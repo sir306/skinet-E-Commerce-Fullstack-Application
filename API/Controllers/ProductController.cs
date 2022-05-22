68525c4ba48edfac78ddf725431c71cc4ece775c
sir306 <hardingnick00@gmail.com>
2022-05-22 18:32:26 +1200

    created interface
    created an interface and started implementing a repository pattern

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -8,6 +8,7 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -15,16 +16,18 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private StoreContext _context;
-        public ProductController(StoreContext context)
+        private readonly IProductRepository _repo;
+        
+        public ProductController(IProductRepository repo)
         {
-            _context = context;
+           _repo = repo;
+          
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
 
             return Ok(products);
         }
@@ -33,7 +36,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
